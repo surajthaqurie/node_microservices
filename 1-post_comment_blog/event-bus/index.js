befb5de7f0ca30fae1c537bd90d1b9fce78f2bfe
Suraj <surajchan68@gmail.com>
2023-06-25 18:40:38 +0545

    demo project for basic working mechanism of  microservices

---
+++ 1-post_comment_blog/event-bus/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// event bus data store
+const events = [];
+
+app.post("/events", async (req, res) => {
+  const event = req.body;
+
+  events.push(event);
+
+  await axios.post("http://localhost:4000/events", event); // post service
+  await axios.post("http://localhost:4001/events", event); // comment service
+  await axios.post("http://localhost:4002/events", event); // query service
+  await axios.post("http://localhost:4003/events", event); // moderation comment service
+
+  res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
+});
+
+app.listen(4005, () => {
+  console.log(`Listening on 4005`);
+});
