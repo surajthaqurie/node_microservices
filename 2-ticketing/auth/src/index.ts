e6e99c69b92aa8d96bbb5d4528903a2ededf0dd4
Suraj <surajchan68@gmail.com>
2023-07-11 23:01:59 +0545

    feat: add route of signup, current user, signin and signout with express validator

--- 2-ticketing/auth/src/index.ts
+++ 2-ticketing/auth/src/index.ts
@@ -1,12 +1,18 @@
 import express from "express";
 
+import { currentUserRouter } from "./routes/currentUser";
+import { signupRouter } from "./routes/signup";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+
 const app = express();
 app.use(express.json());
 
-app.get("/api/users/current-user", (req, res) => {
-  res.send("Hi there!");
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000 !@!");
+  console.log("Listening on port 3000 !!");
 });
