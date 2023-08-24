fe2acafeaaa042ed7323613f722eee4473478a2e
Suraj <surajchan68@gmail.com>
2023-08-24 22:58:26 +0545

    feat: nats streaming server with concurency

--- 2-ticketing/nats-test/src/listener.ts
+++ 2-ticketing/nats-test/src/listener.ts
@@ -10,7 +10,22 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
 
-  const subscription = stan.subscribe("ticket:created");
+  stan.on("close", () => {
+    console.log("NATS connection closed !");
+    process.exit();
+  });
+
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true) // 30 sec automatically
+    .setDeliverAllAvailable() // re-deliver
+    .setDurableName("accounting-service");
+
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "orders-service-queue-group", // queue group name
+    options
+  );
 
   subscription.on("message", (msg: Message) => {
     console.log("Messaged received");
@@ -18,5 +33,10 @@ stan.on("connect", () => {
     if (typeof data === "string") {
       console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
     }
+
+    msg.ack();
   });
 });
+
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
