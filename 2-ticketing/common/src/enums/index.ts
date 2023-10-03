fd64e7a87a07bd399565bafce7e8928ca0f54dc9
Suraj <surajchan68@gmail.com>
2023-10-04 00:13:53 +0545

    feat: add new route with ticket and order realtionship

--- 2-ticketing/common/src/enums/index.ts
+++ 2-ticketing/common/src/enums/index.ts
@@ -1,1 +1,2 @@
 export * from "./subjects.enum";
+export * from "./orderStatus.enum";
--- 2-ticketing/orders/src/routes/new.ts
+++ 2-ticketing/orders/src/routes/new.ts
@@ -1,9 +1,75 @@
+import mongoose from "mongoose";
 import express, { Request, Response } from "express";
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from "@ticketing_microservice/common";
+import { body } from "express-validator";
+
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
-});
+const EXPIRATION_WINDOW_SECONDS = 60 * 15;
+
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("TicketId must be provided"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) throw new NotFoundError();
+
+    // Make sure that the ticket is not already reserved
+    // Run query to look at all orders. Find an order where the ticket
+    // is the ticket we just found *and* the orders status is *not* cancelled.
+    // If we find an order from that means the ticket *is* reserved
+
+    const existingOrder = await Order.findOne({
+      ticket: ticket,
+      status: {
+        $in: [
+          OrderStatus.Created,
+          OrderStatus.AwaitingPayment,
+          OrderStatus.Complete,
+        ],
+      },
+    });
+
+    if (existingOrder) throw new BadRequestError("Ticket is already reserved");
+
+    // Calculate the expiration data for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    // Build the order and save it to the database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+
+    await order.save();
+
+    // Publish an event saying that an order was created
+
+    res.status(201).send(order);
+  }
+);
 
 export { router as newOrderRouter };
