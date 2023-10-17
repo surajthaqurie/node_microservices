67a258085de58e34328337848ac9c6286f2100dc
Suraj <surajchan68@gmail.com>
2023-10-17 20:16:36 +0545

    feat: added startup message

--- 2-ticketing/auth/src/index.ts
+++ 2-ticketing/auth/src/index.ts
@@ -3,6 +3,8 @@ import { app } from "./app";
 
 const start = async () => {
   try {
+    console.log("Starting up...");
+
     if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined");
     if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
 
