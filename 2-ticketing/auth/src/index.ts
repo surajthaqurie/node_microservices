4bfb6cf902f81cc09cba01b723bdcd2e4f1fdee8
Suraj <surajchan68@gmail.com>
2023-10-17 21:11:50 +0545

    chore: update index.ts

--- 2-ticketing/auth/src/index.ts
+++ 2-ticketing/auth/src/index.ts
@@ -2,12 +2,11 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
+  console.log("Starting up...");
+
+  if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined");
+  if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
   try {
-    console.log("Starting up...");
-
-    if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined");
-    if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
-
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("connected to mongodb...");
